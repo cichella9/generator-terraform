16908456b8b097b29daac81818bb0883eaab049d
Gianluigi Mucciolo <mug@MBPdiGianluigi.lan>
2019-07-15 11:57:26 +0200

    fix

--- generators/app/index.js
+++ generators/app/index.js
@@ -213,7 +213,7 @@ module.exports = class extends Generator {
             }
           );
           this.fs.copyTpl(
-            this.templatePath('nvironments/subgroups/regions/outputs.tf'),
+            this.templatePath('environments/subgroups/regions/outputs.tf'),
             this.destinationPath(
               `${environment}/${subgroup}/${region}/outputs.tf`
             ),
